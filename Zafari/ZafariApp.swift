1ba5835cac7829ea331161033be67b3edd951ede
oguzkas <oguzgkas@gmail.com>
2023-10-31 00:35:43 +0300

    Initial Commit

---
+++ Zafari/ZafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZafariApp.swift
+//  Zafari
+//
+//  Created by Oğuz  on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
